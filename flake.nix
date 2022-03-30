7947ff868c2a57c5528761f8181102521985f5ef
a-kenji <aks.kenji@protonmail.com>
2022-03-30 12:45:46 +0200

    init example

--- flake.nix
+++ flake.nix
@@ -1,37 +1,55 @@
 {
-  description = "How to buil wasm modules in nix";
+  description = "How to build wasm modules in nix";
   inputs.rust-overlay.url = "github:oxalica/rust-overlay";
   inputs.rust-overlay.inputs.nixpkgs.follows = "nixpkgs";
   inputs.flake-utils.url = "github:numtide/flake-utils";
   inputs.flake-utils.inputs.nixpkgs.follows = "nixpkgs";
 
-  outputs = { self, nixpkgs , rust-overlay , flake-utils }:
-    flake-utils.lib.defaultSystems (
+  outputs = {
+    self,
+    nixpkgs,
+    rust-overlay,
+    flake-utils,
+  }:
+    flake-utils.lib.eachDefaultSystem (
       system: let
-        overlays = [ (import rust-overlay )];
-          pkgs = import nixpkgs {inherit system overlays;};
-          rustToolchainToml = pkgs.rust-bin.fromRustupToolchainFile ./rust-toolchain;
-  #cargoLock = {
-    #lockFile = builtins.path {
-      #path = ./Cargo.lock;
-      #name = "Cargo.lock";
-    #};
-  #};
-  cargo = rustToolchainToml;
-  rustc = rustToolchainToml;
-  buildInputs = [
-    rustToolchainToml
-  ];
-      in
-      {
-        #packages.wasm-test = (pkgs.makeRustPlatform {inherit cargo rustc}).buildRustPackage {
-          #inherit
-          #src
-          #name
+        src = ./.;
+        overlays = [(import rust-overlay)];
+        pkgs = import nixpkgs {inherit system overlays;};
+        pkgs-cross-wasm-wasi = import nixpkgs {
+      inherit system ;
+      crossSystem = nixpkgs.lib.systems.examples.wasi32
+      // {
+        rustc.config = "wasm32-wasi";
+    };
+    useLLVM = true;
+};
+        rust-cross-wasm-wasi-ToolchainToml = pkgs.rust-bin.fromRustupToolchainFile ./rust-toolchain;
 
-        #};
-        #defaultPackage = packages.wasm-test;
-        devShell = pkgs.mkShell {
-          inherit buildInputs;
+        cargoLock = {
+          lockFile = builtins.path {
+            path = ./Cargo.lock;
+            name = "Cargo.lock";
+          };
         };
-    });}
+        nativeBuildInputs = [
+          rust-cross-wasm-wasi-ToolchainToml
+        ];
+      in rec {
+        packages.wasm-test = (pkgs-cross-wasm-wasi.makeRustPlatform {
+          cargo = rust-cross-wasm-wasi-ToolchainToml;
+          rustc = rust-cross-wasm-wasi-ToolchainToml;
+        }).buildRustPackage {
+          inherit
+            src
+            cargoLock
+            ;
+          name = "wasm-test";
+        };
+        defaultPackage = packages.wasm-test;
+        devShell = pkgs.mkShell {
+          inherit nativeBuildInputs;
+        };
+      }
+    );
+}
